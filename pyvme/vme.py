26c49b0ee03916d1e4b3177ffe8786af978e6cb1
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2020-09-28 06:48:05 +0200

    Allow specifying register width when reading

--- pyvme/vme.py
+++ pyvme/vme.py
@@ -1,3 +1,6 @@
+from ._vmetypes import DataWidth
+
+
 class VMEModule:
     def __init__(self, controller, address):
         self.controller = controller
@@ -9,8 +12,8 @@ class VMEModule:
     def __exit__(self, type_, value, traceback):
         del self.controller
 
-    def read(self, address):
-        return self.controller.read(self.base_address + address)
+    def read(self, address, width=DataWidth.D16):
+        return self.controller.read(self.base_address + address, width=width)
 
     def read_string(self, address_start, address_end):
         return self.controller.read_string(
