3a3645e58337c44f094d58689453f6ff981c18ca
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2022-08-05 13:08:42 +0200

    Update setup.py and structure

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="pyvme",
     version="0.1",
     author="Friedemann Neuhaus",
     author_email="friedemann@neuhaus-tech.de",
     description="Library to control VME devices over the CAEN bridge",
     url="https://github.com/fneuhaus/pyvme",
-    packages=[
-        "pyvme",
-    ],
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
 )
