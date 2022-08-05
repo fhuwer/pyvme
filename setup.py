491605dccd0a2e58c5133262a4680bbb3b6eb525
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2022-08-05 11:09:19 +0200

    Update setup.py to include author, etc.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name="pyvme",
     version="0.1",
+    author="Friedemann Neuhaus",
+    author_email="friedemann@neuhaus-tech.de",
+    description="Library to control VME devices over the CAEN bridge",
+    url="https://github.com/fneuhaus/pyvme",
     packages=[
         "pyvme",
     ],
